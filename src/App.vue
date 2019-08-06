bfc97f0fe9396493348d395bfe44ed91aa4bb699
Natacha Romano <natacharomanonr@gmail.com>
2019-08-06 11:02:06 -0300

    componente imagem

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,11 @@
 <template>
   <div class="corpo">
     <h1 class="texto-centralizado">{{ titulo }}</h1>
+    <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre por parte do título">
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
-            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
         </meu-painel>
       </li>
     </ul>
@@ -15,18 +16,34 @@
 
 <script>
 import Painel from './components/shared/painel/Painel.vue';
+import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue'
 
 export default {
 
   components: {
-    'meu-painel': Painel
+    'meu-painel': Painel,
+    'imagem-responsiva': ImagemResponsiva
   },
 
   data() {
     return {
       titulo: 'Alurapic',
-      fotos: []
+      fotos: [],
+      filtro: ''
     }
+  },
+
+  computed: {
+
+    fotosComFiltro() {
+      if (this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
+    }
+
   },
 
   created() {
@@ -57,7 +74,8 @@ export default {
       display: inline-block;
     }
 
-  .imagem-responsiva {
+  .filtro {
+    display: block;
     width: 100%;
   }
 
--- src/components/shared/painel/Painel.vue
+++ src/components/shared/painel/Painel.vue
@@ -3,8 +3,12 @@
 <template>
 
     <div class="painel">
-        <h2 class="painel-titulo">{{ titulo }}</h2>
-        <slot class="painel-conteudo"></slot>
+        <h2 class="painel-titulo" @dblclick="visivel = !visivel">{{ titulo }}</h2>
+        <transition name="painel-fade">
+            <div class="painel-conteudo" v-show="visivel">
+                <slot></slot>
+            </div>
+        </transition>
     </div>
 
 </template>
@@ -12,7 +16,13 @@
 <script>
 export default {
 
-    props: ['titulo']
+    props: ['titulo'],
+
+    data() {
+        return {
+            visivel: true
+        }
+    }
 
 }
 </script>
@@ -42,4 +52,15 @@ export default {
     text-transform: uppercase;
   }
 
+  * {
+      box-shadow: 5px 5px 5px;
+  }
+
+  .painel-fade-enter, .painel-fade-leave-active {
+      opacity: 0;
+  }
+
+    .painel-fade-enter-active, .painel-fade-leave-active {
+        transition: opacity .4s;
+    }
 </style>
