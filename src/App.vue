3cc595a72f2465198dfadc742afd6d378f0b0927
Natacha Romano <natacharomanonr@gmail.com>
2019-08-06 10:20:11 -0300

    componente

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
+
+
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+  <div class="corpo">
+    <h1 class="texto-centralizado">{{ titulo }}</h1>
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos">
+        <meu-painel :titulo="foto.titulo">
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
+
+  components: {
+    'meu-painel': Painel
+  },
 
   data() {
     return {
@@ -28,5 +38,27 @@ export default {
 </script>
 
 <style>
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+
+    .texto-centralizado {
+      text-align: center;
+    }
+
+    .lista-fotos {
+      list-style: none;
+    }
+  
+    .lista-fotos .lista-fotos-item {
+      display: inline-block;
+    }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
 
 </style>
