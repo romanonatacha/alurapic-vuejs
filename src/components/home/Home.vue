f45a4452b6d134387ea7903966e222df7a7b79de
Natacha Romano <natacha.romano@valtech.com>
2019-08-08 15:28:30 -0300

    componente botao

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -8,6 +8,13 @@
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
           <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+          <meu-botao
+            tipo="button"
+            rotulo="REMOVER"
+            @botaoAtivado="remove(foto)"
+            :confirmacao="false"
+            estilo="perigo"
+          />
         </meu-painel>
       </li>
     </ul>
@@ -16,13 +23,15 @@
 
 <script>
 import Painel from '../shared/painel/Painel.vue';
-import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue';
+import Botao from '../shared/botao/Botao.vue';
 
 export default {
 
   components: {
     'meu-painel': Painel,
-    'imagem-responsiva': ImagemResponsiva
+    'imagem-responsiva': ImagemResponsiva,
+    'meu-botao': Botao
   },
 
   data() {
@@ -42,6 +51,14 @@ export default {
       } else {
         return this.fotos;
       }
+    }
+
+  },
+
+  methods: {
+
+    remove(foto) {
+      alert('Remover a foto!' + foto.titulo);
     }
 
   },
