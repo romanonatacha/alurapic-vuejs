4eb75a7dd13922191305f9d799a08960dc2616be
Natacha Romano <natacharomanonr@gmail.com>
2019-08-12 11:38:13 -0300

    diretiva

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -7,7 +7,7 @@
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
-          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+          <imagem-responsiva v-meu-transform:scale.animate="1.2" :url="foto.url" :titulo="foto.titulo"/>
           <meu-botao
             tipo="button"
             rotulo="REMOVER"
--- src/components/shared/painel/Painel.vue
+++ src/components/shared/painel/Painel.vue
@@ -63,4 +63,8 @@ export default {
     .painel-fade-enter-active, .painel-fade-leave-active {
         transition: opacity .4s;
     }
+
+    .painel-conteudo {
+        overflow: hidden;
+    }
 </style>
