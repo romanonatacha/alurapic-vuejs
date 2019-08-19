8461d3d05f9f1931583eff846c601bbf16fcfac4
Natacha Romano <natacharomanonr@gmail.com>
2019-08-19 16:01:23 -0300

    validando formularios

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -10,12 +10,14 @@
     <form @submit.prevent="grava()">
       <div class="controle">
         <label for="titulo">TÍTULO</label>
-        <input id="titulo" autocomplete="off" v-model.lazy="foto.titulo">
+        <input data-vvv-as="título" name="titulo" v-validate data-vv-rules="required|min:3|max:30" id="titulo" autocomplete="off" v-model="foto.titulo">
+        <span class="erro" v-show="errors.has('titulo')">{{ errors.first('titulo') }}</span>
       </div>
 
       <div class="controle">
         <label for="url">URL</label>
-        <input id="url" autocomplete="off" v-model.lazy="foto.url">
+        <input name="url" v-validate data-vv-rules="required" id="url" autocomplete="off" v-model="foto.url">
+        <span class="erro" v-show="errors.has('url')">{{ errors.first('url') }}</span>
         <imagem-responsiva v-show="foto.url" :url="foto.url" :titulo="foto.titulo" />
       </div>
 
@@ -62,12 +64,19 @@ export default {
 
       grava () {
 
-        this.service
-          .cadastra(this.foto)
-          .then(() => {
-            if (this.id) this.$router.push({ name: 'home' })
-            this.foto = new Foto();
-          }, err => console.log(err))
+        this.$validator
+          .validateAll()
+          .then(success => {
+            if (success) {
+              this.service
+                .cadastra(this.foto)
+                .then(() => {
+                  if (this.id) this.$router.push({ name: 'home' })
+                  this.foto = new Foto();
+                }, err => console.log(err))
+            }
+          });
+
         
       }
   },
@@ -107,6 +116,10 @@ export default {
 
   .centralizado {
     text-align: center;
+  }
+
+  .erro {
+    color: red;
   }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
 import { routes } from './routes';
 import './directives/Transform';
+import VeeValidate from 'vee-validate';
+import msg from './pt_BR';
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000'
@@ -13,6 +15,17 @@ Vue.use(VueRouter);
 const router = new VueRouter({
   routes,
   mode: 'history'
+ });
+
+ Vue.use(VeeValidate, {
+   locale: 'pt_BR',
+   dictionary: {
+
+    pt_BR: {
+
+      messages: msg
+    }
+   }
  });
 
 new Vue({
